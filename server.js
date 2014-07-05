ff18ff6b9f33cd9a5bb33e8002950ac1751e7516
Jasen Lew <jasen.lew@gmail.com>
2014-07-05 15:58:05 -0700

    Add colon on console.log for server.js

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var app = require('./server-config.js');
 
-var port = 4568;
+// App will be able to be run both locally and when deployed; using below will create redundant system
+var port = process.env.PORT || 4568;
 
 app.listen(port);
 
-console.log('Server now listening on port ' + port);
+console.log('Server now listening on port: ' + port);
